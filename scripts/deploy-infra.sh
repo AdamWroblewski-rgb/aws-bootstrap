a19def8fd04a04c4ff47facd7c56aeff8e9527a8
Adam W <awroblewski77@gmail.com>
2024-10-14 23:00:26 +0200

    add codeploy spec

--- scripts/deploy-infra.sh
+++ scripts/deploy-infra.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
+source aws_credentials.sh
 
 STACK_NAME=awsbootstrap
-REGION=us-east-1
+REGION=eu-north-1
 CLI_PROFILE=awsbootstrap
 
 EC2_INSTANCE_TYPE=t3.micro
 
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --profile awsbootstrap \
+	--query "Account" --output text)
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+# Deploys static resources
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 echo -e "\n\n================== Deploying main.yml ============"
-aws cloudformation deploy\
-	--region $REGION \
+aws cloudformation deploy --region $REGION \
 	--profile $CLI_PROFILE \
 	--stack-name $STACK_NAME \
 	--template-file main.yml \
@@ -17,8 +33,8 @@ aws cloudformation deploy\
 	--parameter-overrides \
 	EC2InstanceType=$EC2_INSTANCE_TYPE
 
-if [[ $? -eq 0 ]];then
+if [[ $? -eq 0 ]]; then
 	aws cloudformation list-exports \
-	--profile awsbootstrap \
-	--query "Exports[?Name=='InstanceEndpoint'].Value"
+		--profile awsbootstrap \
+		--query "Exports[?Name=='InstanceEndpoint'].Value"
 fi
